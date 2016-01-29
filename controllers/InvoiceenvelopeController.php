38eaff541ce449809345aa810d52aad9deb72718
Mihaly Sallai <mihaly.sallai@frontside.hu>
2016-01-29 10:41:02 -0700

    fixes

---
+++ controllers/InvoiceenvelopeController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use app\models\Client;
+use app\models\Invoice;
+use app\models\News;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use app\components\BaseController;
+use app\models\PaymentMethod;
+use app\models\InvoiceGroup;
+use app\models\InvoiceGroupItem;
+use kartik\mpdf\Pdf;
+use app\components\NumberToString;
+use yii\filters\AccessControl;
+
+use yii\db\Query;
+
+class InvoiceenvelopeController extends BaseController
+{
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['invoiceManager'],
+                    ],
+                ],
+            ],
+        ];
+    }
+    
+    public function actionIndex()
+    {
+        $this->layout = 'invoice-pdf';
+        
+        $invoiceIds = [];
+        if (isset($_REQUEST['id']) && $_REQUEST['id']) {
+            $invoiceIds[] = $_REQUEST['id'];
+        }
+        elseif (isset($_REQUEST['invoice_group_id']) && $_REQUEST['invoice_group_id']) {
+            if (($invoiceGroup = InvoiceGroup::findOne($_REQUEST['invoice_group_id'])) !== null) {
+                $invoiceIds = $invoiceGroup->invoiceIds;
+            } else {
+                throw new NotFoundHttpException('The requested page does not exist.');
+            }
+        }
+        
+        if (!count($invoiceIds)) {
+            Yii::$app->getSession()->setFlash('danger', Yii::t('app','Invalid invoice id') );
+            return $this->render('pdf-error');
+        }
+        
+        $dataArray = [];
+        foreach( $invoiceIds as $invoice_id ) {
+            $invoice    = Invoice::findOne($invoice_id);
+            $dataSet    = unserialize($invoice->invoice_data);          
+            $dataArray[$dataSet['client']->id] = $dataSet['client'];            
+        }
+        
+        $formats = ['LC5'=>[162,229],'LC6'=>[114,162]];
+        $format = 'LC5';
+        if (isset($_REQUEST['format']) && $_REQUEST['format']) {
+            $format = $_REQUEST['format'];
+        }
+        
+        $content = $this->render('pdf-envelope',['dataArray'=>$dataArray,'format_'=>$format]); 
+        
+        $pdf = new Pdf([
+            'mode'          => Pdf::MODE_UTF8,
+            'format'        => $formats[$format], //162x229 LC5  //Pdf::FORMAT_A4,
+            'orientation'   => Pdf::ORIENT_LANDSCAPE,
+            'destination'   => Pdf::DEST_BROWSER,
+            'content'       => $content,
+            'cssFile'       => '',
+            'cssInline'     => '',
+            'options'       => ['title' => ''],
+            'methods'       => []
+        ]);
+
+        Yii::$app->response->format = \yii\web\Response::FORMAT_RAW;
+        $headers = Yii::$app->response->headers;
+        $headers->add('Content-Type', 'application/pdf');
+        
+        return $pdf->render();
+    }
+    
+}
---
+++ controllers/InvoicegroupController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use app\models\Client;
+use app\models\Invoice;
+use app\models\InvoiceSearch;
+use app\models\InvoiceItem;
+use app\models\InvoiceGroup;
+use app\models\InvoiceGroupItem;
+use app\models\News;
+use app\models\NewsSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use app\components\BaseController;
+use app\models\District;
+use app\models\DistrictSearch;
+use app\models\NewsDistrict;
+use app\models\PaymentMethod;
+use kartik\mpdf\Pdf;
+use app\components\NumberToString;
+use yii\filters\AccessControl;
+use yii\data\ActiveDataProvider;
+
+use yii\db\Query;
+
+class InvoicegroupController extends BaseController
+{
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['invoiceManager'],
+                    ],
+                ],
+            ],
+        ];
+    }
+    
+    public function actionIndex()
+    {        
+        $searchModel = new InvoiceGroup();
+        $dataProvider = $searchModel->search(Yii::$app->request->bodyParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+    
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+        
+    }
+
+    protected function findModel($id)
+    {
+        if (($model = InvoiceGroup::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+}
